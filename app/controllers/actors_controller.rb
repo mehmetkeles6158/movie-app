e0526a46dc5020aa27627cbd4cbe1e1a09872346
Mehmet <mehmetk6158@gmail.com>
2021-10-13 11:03:44 -0400

    routes, webrequest,called first actor,create a method

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,14 @@
 class ActorsController < ApplicationController
+  def firstactor
+
+    first = Actor.first
+
+    fname = first.first_name
+    lname = first.last_name
+
+    render json:{first_actor: first}
+
+    # render json:{message:"#{fname} #{lname} is my favorite actor!"}
+    
+  end
 end
