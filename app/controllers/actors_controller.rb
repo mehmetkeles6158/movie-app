fd76d5ffd553c2b66fee3792ad08924bc8819df7
Mehmet <mehmetk6158@gmail.com>
2021-10-13 11:34:41 -0400

    added 10 fake actors to db

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -11,4 +11,18 @@ class ActorsController < ApplicationController
     # render json:{message:"#{fname} #{lname} is my favorite actor!"}
     
   end
+
+  def all_actors
+    # this code is in seed.rb
+    # 10.times do
+    #   actor = Actor.new(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name, known_for:Faker::Superhero.descriptor)
+    #   actor.save
+    # end
+
+    actors = Actor.all
+
+    render json:actors.as_json
+  end
+
+
 end
