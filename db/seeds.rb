78db90bd722eab256ff7a1be3c54aa41c8982f64
Mehmet <mehmetk6158@gmail.com>
2021-10-26 16:52:30 -0400

    created one to many association in movie-app

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
-actor.save
+# actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+# actor.save
 
-10.times do
-  actor = Actor.new(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name, known_for:Faker::Superhero.descriptor)
-  actor.save
-end
+# 10.times do
+#   actor = Actor.new(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name, known_for:Faker::Superhero.descriptor)
+#   actor.save
+# end
