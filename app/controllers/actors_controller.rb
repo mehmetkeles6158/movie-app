d98990fe42aed331ad98680564451f2578ce538b
Mehmet <mehmetk6158@gmail.com>
2021-10-27 15:59:28 -0400

    created authorization

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,36 +1,30 @@
 class ActorsController < ApplicationController
-  def firstactor
-
-    first = Actor.first
-
-    fname = first.first_name
-    lname = first.last_name
-
-    render json:{first_actor: first}
-
-    # render json:{message:"#{fname} #{lname} is my favorite actor!"}
-    
-  end
-
-  def all_actors
-    # this code is in seed.rb
-    # 10.times do
-    #   actor = Actor.new(first_name:Faker::Name.first_name, last_name:Faker::Name.last_name, known_for:Faker::Superhero.descriptor)
-    #   actor.save
-    # end
-
+  def index
     actors = Actor.all
-
-    render json:actors.as_json
-  end
-
-  def actor_method
-
-    actor1 = params["actor"]
-
-    render json:{message:"My second favorite actor is #{actor1}!"}
+    render json:{all_actors: actors}
     
   end
 
+  def show
+
+    actor = Actor.find_by(id:params[:id])
+    render json:{actor: actor}
+    
+  end
+
+  def create
+    actor = Actor.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for]
+    )
+
+    if actor.save
+      render json:{actor_created: actor}
+    else
+      render json: {errors: actor.errors.full_messages}
+    end
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/actor", controller: "actors", action:"firstactor"
-  get "/allactors", controller: "actors", action:"all_actors"
-  get "/query_single_actor" =>"actors#actor_method"
-  get "/url_single_actor/:actor" =>"actors#actor_method"
-  post "/body_single_actor" => "actors#actor_method"
+  get "/actors" => "actors#index"
+  get "/actors/:id" => "actors#show"
+  post "/actors" => "actors#create"
 
-  post "/users" => "users#create"
+  
   post "/sessions" => "sessions#create"
 
 
