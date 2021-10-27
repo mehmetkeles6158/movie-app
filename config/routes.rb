26c3a2795efa674a01c72ca465fd205e3531df81
Mehmet <mehmetk6158@gmail.com>
2021-10-27 15:16:20 -0400

    created user model, authentication part-1, signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/actor", controller: "actors", action:"firstactor"
-
   get "/allactors", controller: "actors", action:"all_actors"
-
   get "/query_single_actor" =>"actors#actor_method"
-
   get "/url_single_actor/:actor" =>"actors#actor_method"
-
   post "/body_single_actor" => "actors#actor_method"
+
+  post "/users" => "users#create"
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_203731) do
+ActiveRecord::Schema.define(version: 2021_10_27_190707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2021_10_26_203731) do
     t.string "title"
     t.integer "year"
     t.text "plot"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
