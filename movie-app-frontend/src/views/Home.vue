51b6ef4750be9b6c3c3afbe6c1e654a2b89ae67f
Mehmet <mehmetk6158@gmail.com>
2021-11-16 12:55:58 -0500

    add create action

--- movie-app-frontend/src/views/Home.vue
+++ movie-app-frontend/src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <p> {{ actors }}</p>
+    <p>
+      <b>First Name:</b>
+      <input type="text" v-model="newActor.first_name" />
+    </p>
+    <p>
+      <b>Last Name:</b>
+      <input type="text" v-model="newActor.last_name" />
+    </p>
+    <p>
+      <b>known_for:</b>
+      <input type="text" v-model="newActor.known_for" />
+    </p>
+    <button v-on:click="addActor">Add New Actor</button>
+
+    <h1>Actors</h1>
+    <div v-for="actor in actors">
+      <p>{{ actor.first_name }} {{actor.last_name}}</p>
+    </div>
       </p>
     </div>
+    
   </div>
 </template>
 
@@ -16,6 +34,7 @@ export default {
     return {
       message: "Welcome to Movie-app!",
       actors: [],
+      newActor: {},
     }
   },
   created: function () {
@@ -24,11 +43,23 @@ export default {
   methods: {
     indexActors: function () {
       console.log("loading actors");
-      axios.get("http://localhost:3000/actors").then((response) => {
+      axios.get("/actors").then((response) => {
         console.log(response.data);
         this.actors = response.data;
       });
+
     },
+    addActor: function() {
+      console.log("adding new actor!");
+      axios.post("/actors", {
+        first_name: this.newActor.first_name,
+        last_name: this.newActor.last_name,
+        known_for: this.newActor.known_for,
+      }).then((response)=>{
+        console.log(response.data);
+        console.log(this.newContact);
+      })
+    }
   },
 };
 </script>
